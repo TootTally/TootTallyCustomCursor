b48e87744f9784707b4567f0602a2906c445f5d1
Electrostats <Super_justy@hotmail.com>
2024-02-08 07:59:55 -0500

    Fixed aspect ratio check being inverted

--- CustomCursor.cs
+++ CustomCursor.cs
@@ -148,7 +148,7 @@ namespace TootTallyCustomCursor
             float trailSpeed = Plugin.Instance.TrailSpeed.Value;
             if (Plugin.Instance.TrailAdjustTrailSpeed.Value) //If this isn't the trail preview and trail has to be auto adjusted
             {
-                float aspectRatioMult = GlobalVariables.testScreenRatio() == 1610 ? 1f : 0.9f; //_StaticR-atl - Account for scroll speed being affected by aspect ratio.
+                float aspectRatioMult = GlobalVariables.testScreenRatio() == 1610 ? .9f : 1f; //_StaticR-atl - Account for scroll speed being affected by aspect ratio.
                 scalerModifier = __instance.tempo * TootTallyGlobalVariables.gameSpeedMultiplier * __instance.defaultnotelength / 40600f * aspectRatioMult;
                 trailSpeed = 17f * scalerModifier; //Hard coded 17f trail speed
             }
